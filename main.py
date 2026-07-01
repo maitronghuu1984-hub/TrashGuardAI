655b1bdc7d538d2bc1abcdd8edb383d9c4a76142
maitronghuu <maitronghuu@gmail.com>
2026-07-01 23:26:12 +0700

    Fix Render start command

--- main.py
+++ main.py
@@ -139,9 +139,11 @@ async def analyze(request: Request):
 
 
 if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 8000))
+
     uvicorn.run(
         "main:app",
         host="0.0.0.0",
-        port=8000,
-        reload=True
+        port=port
     )
